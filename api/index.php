b71f026c2b71ae5028ef72ab05294c0612b3ad90
Gitt_Server <104231713+GittServer@users.noreply.github.com>
2024-03-03 08:41:41 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,19 +2,3 @@
 header('content-type: application/json');
 echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
 ?> 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-<!-- https://youtu.be/30Of7BFeGHI -->
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <h1>Acesse sua conta</h1>
-    <a href="index_login.php">VOLTAR LOGIN</a>
-</body>
-</html>
